7d55acd4b7e3d9fbdeccdfaa60ff600fbb79ca51
unknown <niko.stratil@gmail.com>
2022-09-06 10:02:26 +0200

    Added data set and documentation for euclidean and wiki_graph

--- R/euclidean.R
+++ R/euclidean.R
@@ -1,3 +1,16 @@
+#' Calculate the gcd of two numbers using the euclidean algorithm
+#'
+#' This functions calculates the greatest common denominator (gcd) of two numbers
+#' using the euclidean algorithm.
+#' For specifics about the algorithm, refer to the wikipedia page
+#' \url{https://en.wikipedia.org/wiki/Euclidean_algorithm}
+#'
+#' @param a Number a
+#' @param b Number b
+#' @return The gcd of \code{a} and \code{b}.
+#' @examples
+#' euclidean(1000, 100)
+#' euclidean(250, 55)
 euclidean <- function(a,b){
   stopifnot(is.numeric(a), is.numeric(b), length(a) == 1, length(b) == 1)
   if(a>b){
---
+++ R/wiki_graph-data.R
@@ -0,0 +1,12 @@
+#' This data set provides an example graph to easily test the dijkstra() function
+#'
+#' @usage data(wiki_graph)
+#'
+#' @format A data frame with 18 rows and 3 variables:
+#' \describe{
+#'  \item{v1}{starting point of the edge}
+#'  \item{v2}{end point of the edge}
+#'  \item{w}{weight of the edge}
+#' }
+#' @source The graph was provided in the assignment for this lab.
+"wiki_graph"
